33164eae9a2d3f1d485e065e5121e0df60503a5b
JonniBig <bigun.i88@gmail.com>
2023-08-04 15:53:12 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+let intervalID = null;
+stopBtn.disabled = true;
 
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', e => {
+  intervalID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 100);
+  e.target.setAttribute('disabled', true);
+  e.target.nextElementSibling.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', e => {
+  clearInterval(intervalID);
+  e.target.setAttribute('disabled', true);
+  e.target.previousElementSibling.removeAttribute('disabled');
+});
